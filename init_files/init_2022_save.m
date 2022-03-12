9f80fc3bbf194ff03ee5f96ccfe2928f83dbbb2e
Conor Cosnett <conorcosnett@gmail.com>
2022-03-12 19:07:39 +0000

    saving current init.m

---
+++ init_files/init_2022_save.m
@@ -0,0 +1,93 @@
+AppendTo[$Path, "/home/conor/Dropbox/05_PROGRAMS/03_PARSER/"];
+AppendTo[$Path, "/home/conor/Dropbox/05_PROGRAMS/19_brain_computer_interface/01_MIND_EXTENSION/mindExtension/"];
+AppendTo[$Path, "/home/conor/Dropbox/05_PROGRAMS/19_brain_computer_interface/02_SHORTCUT_LIST/shortcutList/"];
+AppendTo[$Path, "/home/conor/Dropbox/05_PROGRAMS/000_hyper_graph/"];
+AppendTo[$Path,"/home/conor/Dropbox/05_PROGRAMS/14_vector_calculus_functions/"];
+
+AppendTo[$Path, "/home/conor/Dropbox/05_PROGRAMS/12_statistical_functions/"];
+AppendTo[$Path, "/home/conor/Dropbox/05_PROGRAMS/06_Linear_Algebra_Functions/"];
+AppendTo[$Path, "/home/conor/Dropbox/05_PROGRAMS/17_steno/steno/"];
+(*Get["/home/conor/Dropbox/05_PROGRAMS/17_steno/steno/stenoLinux.m"];*)
+(*Get["/home/conor/Dropbox/05_PROGRAMS/12_statistical_functions/statistical.m"];*)
+(*Get["/home/conor/Dropbox/05_PROGRAMS/07_Logic_Functions/truthtabler.m"];*)
+AppendTo[$Path, "/home/conor/Dropbox/05_PROGRAMS/07_Logic_Functions/"];
+AppendTo[$Path, "/home/conor/Dropbox/05_PROGRAMS/27_Russian/"];
+
+AppendTo[$Path, "/home/conor/Dropbox/07_liquidity/"];
+AppendTo[$Path, "/home/conor/Dropbox/07_liquidity/43_just_total/"];
+AppendTo[$Path, "/home/conor/Dropbox/07_liquidity/44_basement/"];
+
+
+
+$do=FrontEndTokenExecute;
+do=FrontEndTokenExecute;
+
+(*Get["liq`"];*)
+Get["mindExtension`"];
+Get["abilities`"];
+Get["hyperGraph`"];
+Get["cephalon`"];
+Get["cel`"];
+
+
+
+(*Get["limitOfTheRatio`"];*)
+Get["RussianTools`"];
+
+
+(*Get["lin`"];*)
+(*Get["stenoLinux`"];*)
+
+(*Get["logical`"];*)
+
+
+(*Get["truthtree1`"];*)
+(*Get["stenotypy`"]*)
+
+
+(*Get["parser`"]*)
+
+
+
+
+
+
+
+(****************************************************************)
+(****************************************************************)
+(*AppendTo[$Path, "~/Dropbox/05_PROGRAMS/000_hyper_graph/"];*)
+(*AppendTo[$Path, "~/Dropbox/05_PROGRAMS/000_hyper_graph/mmaEdinburgh/"];*)
+
+
+AppendTo[$Path, "~/Dropbox/05_PROGRAMS/19_brain_computer_interface/02_SHORTCUT_LIST/shortcutList/"];
+AppendTo[$Path, "~/Dropbox/05_PROGRAMS/19_brain_computer_interface/"];
+AppendTo[$Path, "~/Dropbox/05_PROGRAMS/19_brain_computer_interface/01_MIND_EXTENSION/mindExtension/"];
+AppendTo[$Path, "~/Dropbox/05_PROGRAMS/19_brain_computer_interface/03_DECK_OF_JOKERS/"];
+(****************************************************************)
+(****************************************************************)
+(*$Messages = {};*)
+
+
+
+
+AppendTo[$Path, "/home/conor/Dropbox/07_liquidity/91_Liqquid/06_m_model/"];
+AppendTo[$Path, "/home/conor/Dropbox/07_liquidity/91_Liqquid/m_nnpav/"];
+AppendTo[$Path, "/home/conor/Dropbox/07_liquidity/91_Liqquid/max_enhanced_price/"];
+(*AppendTo[$Path, "/home/conor/Dropbox/07_liquidity/91_Liqquid/user/"];*)
+Get["mod`"];
+Get["nnpav`"];
+Get["mep`"];
+(*Get["max`"];*)
+
+
+
+
+
+
+
+
+
+
+
+
+
