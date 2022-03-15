164990ce836b1c743d591d499bab3162359f243c
Conor Cosnett <conorcosnett@gmail.com>
2022-03-15 14:25:51 +0000

    got rid of liqquid_stuff from init file

---
+++ init_files/init.m
@@ -0,0 +1,82 @@
+progs=progs<>"";
+AppendTo[$Path, progs<>"/03_PARSER/"];
+AppendTo[$Path, progs<>"/19_brain_computer_interface/01_MIND_EXTENSION/mindExtension/"];
+AppendTo[$Path, progs<>"/19_brain_computer_interface/02_SHORTCUT_LIST/shortcutList/"];
+AppendTo[$Path, progs<>"/000_hyper_graph/"];
+AppendTo[$Path, progs<>"/14_vector_calculus_functions/"];
+
+AppendTo[$Path, progs<>"/12_statistical_functions/"];
+AppendTo[$Path, progs<>"/06_Linear_Algebra_Functions/"];
+AppendTo[$Path, progs<>"/17_steno/steno/"];
+(*Get[progs<>"/17_steno/steno/stenoLinux.m"];*)
+(*Get[progs<>"/12_statistical_functions/statistical.m"];*)
+(*Get[progs<>"/07_Logic_Functions/truthtabler.m"];*)
+AppendTo[$Path, progs<>"/07_Logic_Functions/"];
+AppendTo[$Path, progs<>"/27_Russian/"];
+(*folders = {"/03_PARSER/"};*)
+
+
+
+$do=FrontEndTokenExecute;
+do=FrontEndTokenExecute;
+
+(*Get["liq`"];*)
+Get["mindExtension`"];
+Get["abilities`"];
+Get["hyperGraph`"];
+Get["cephalon`"];
+Get["cel`"];
+
+
+
+(*Get["limitOfTheRatio`"];*)
+Get["RussianTools`"];
+
+
+(*Get["lin`"];*)
+(*Get["stenoLinux`"];*)
+
+(*Get["logical`"];*)
+
+
+(*Get["truthtree1`"];*)
+(*Get["stenotypy`"]*)
+
+
+(*Get["parser`"]*)
+
+
+
+
+
+
+
+(****************************************************************)
+(****************************************************************)
+(*AppendTo[$Path, progs<>"/000_hyper_graph/"];*)
+(*AppendTo[$Path, progs<>"/000_hyper_graph/mmaEdinburgh/"];*)
+
+
+AppendTo[$Path, progs<>"/19_brain_computer_interface/02_SHORTCUT_LIST/shortcutList/"];
+AppendTo[$Path, progs<>"/19_brain_computer_interface/"];
+AppendTo[$Path, progs<>"/19_brain_computer_interface/01_MIND_EXTENSION/mindExtension/"];
+AppendTo[$Path, progs<>"/19_brain_computer_interface/03_DECK_OF_JOKERS/"];
+(****************************************************************)
+(****************************************************************)
+(*$Messages = {};*)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
